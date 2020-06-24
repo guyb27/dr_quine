50f3979ed76e20d0f0659685382f50a6a9955690
guyb27 <gmadec.student@protonmail.com>
2020-06-25 00:25:20 +0200

    petite modif

--- Grace/Python/Grace.py
+++ Grace/Python/Grace.py
@@ -1,6 +1,6 @@
 #Seulement trois defines sont autorisees
 MODE = "w"
 STDOUT = "Grace_kid.py"
-PRINTABLE = "#Seulement trois defines sont autorisees{0}MODE = {1}w{1}{0}STDOUT = {1}Grace_kid.py{1}{0}PRINTABLE = {1}{2}{1}{0}with open(STDOUT, MODE) as kid:{0}{3}kid.write(PRINTABLE.format(chr(10), chr(34), PRINTABLE,chr(9))){0}"
-with open(STDOUT, MODE) as kid:
-	kid.write(PRINTABLE.format(chr(10), chr(34), PRINTABLE,chr(9)))
+PRINTABLE = "#Seulement trois defines sont autorisees{0}MODE = {1}w{1}{0}STDOUT = {1}Grace_kid.py{1}{0}PRINTABLE = {1}{2}{1}{0}with open(STDOUT, MODE) as f:{0}{3}f.write(PRINTABLE.format(chr(10), chr(34), PRINTABLE,chr(9))){0}"
+with open(STDOUT, MODE) as f:
+	f.write(PRINTABLE.format(chr(10), chr(34), PRINTABLE,chr(9)))

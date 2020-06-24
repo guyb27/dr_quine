caf738c7e34f988401d565e94b5e0fa1f96ddcb5
guyb27 <gmadec.student@protonmail.com>
2020-06-24 15:41:21 +0200

    Avant la verif final

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-/*Hello world!*/
-char s[] = "#include <stdio.h>%1$c%2$c*Hello world!*%2$c%1$cchar s[] = %3$c%4$s%3$c;%1$cvoid ft_print(void)%1$c{%1$cprintf(s,10,47,34,s);%1$c}%1$cint main(void)%1$c{%1$c%2$c*Hello world!*%2$c%1$cft_print();%1$c}";
+/*
+	Hello world!
+*/
+char s[] = "#include <stdio.h>%1$c%2$c*%1$c%5$cHello world!%1$c*%2$c%1$cchar s[] = %3$c%4$s%3$c;%1$cvoid ft_print(void)%1$c{%1$cprintf(s,10,47,34,s,9);%1$c}%1$cint main(void)%1$c{%1$c%2$c*%1$c%5$cHello world!%1$c*%2$c%1$cft_print();%1$c}";
 void ft_print(void)
 {
-printf(s,10,47,34,s);
+printf(s,10,47,34,s,9);
 }
 int main(void)
 {
-/*Hello world!*/
+/*
+	Hello world!
+*/
 ft_print();
 }

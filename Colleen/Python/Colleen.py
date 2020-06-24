2e2b7104332f4bde2240bdafb753e0045cdae09b
guyb27 <gmadec.student@protonmail.com>
2020-06-24 20:17:54 +0200

    colleen finit

---
+++ Colleen/Python/Colleen.py
@@ -0,0 +1,9 @@
+# Hello world
+s = "# Hello world{0}s = {3}{2}{3}{0}def ft_print():{0}{1}print(s.format(chr(10),chr(9),s,chr(34))){0}def main():{0}{1}#Hello world{0}{1}ft_print(){0}if __name__ == {3}__main__{3}:{0}{1}main()"
+def ft_print():
+	print(s.format(chr(10),chr(9),s,chr(34)))
+def main():
+	#Hello world
+	ft_print()
+if __name__ == "__main__":
+	main()
